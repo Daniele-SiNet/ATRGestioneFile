10f4be97c08389ebc46f304f416578e655f8a295
Daniele Pedruzzi <danielep@si-net.it>
2020-12-15 11:26:15 +0100

    prima fase

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ATRGestioneFile
 {
@@ -10,10 +11,20 @@ namespace ATRGestioneFile
     {
         static void Main()
         {
+            Console.WriteLine("************************************");
+            Console.WriteLine("inserisci il percorso da cui leggere i files:");
+            string path = Console.ReadLine();
+            Console.WriteLine("************************************");
+            string[] files = System.IO.Directory.GetFiles(path);
 
-            string a ="a";
+            foreach (string s in files)
+            {
+                Console.WriteLine(s);
+            }
 
 
+
+            Console.ReadKey();
         }
     }
 }
